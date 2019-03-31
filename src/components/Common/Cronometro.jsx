04bd01194066ec934142661d813ec1f9425df289
Leonardo Alfaya Fonseca <leo.alfaya@gmail.com>
2019-03-30 23:02:51 -0300

    Game tests started

--- src/components/Common/Cronometro.jsx
+++ src/components/Common/Cronometro.jsx
@@ -7,7 +7,7 @@ import { openModalResultado } from '../../actions/modalResultadoActions';
 
 const setTempoTermino = () => {
     let tempo = new Date();
-    tempo = tempo.setSeconds(tempo.getSeconds() + 30);
+    tempo = tempo.setMinutes(tempo.getMinutes() + 2);
 
     return tempo
 }
